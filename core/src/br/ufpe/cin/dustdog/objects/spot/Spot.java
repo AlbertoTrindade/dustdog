6ea516769856d3a24510fb714417cd1145d91048
AlbertoTrindade <att@cin.ufpe.br>
2015-01-07 23:19:26 -0300

    Fix bug on handling swipe

--- core/src/br/ufpe/cin/dustdog/objects/spot/Spot.java
+++ core/src/br/ufpe/cin/dustdog/objects/spot/Spot.java
@@ -9,21 +9,21 @@ public class Spot extends DynamicGameObject {
 	public LaneState laneState;
 
 	public float stateTime;
-	
+
 	public static final float SPOT_POSITION_Y = 0.5f;
 	public static final float SPOT_VELOCITY = 5f;
-	
-	public static final float SPOT_WIDTH = 2.37f;
-	public static final float SPOT_HEIGHT = 1.6f;
 
-	public static final float LEFT_LANE_POSITION_X = 1.2f;
-	public static final float CENTRAL_LANE_POSITION_X = 3.65f;
-	public static final float RIGHT_LANE_POSITION_X = 6.1f;
+	public static final float SPOT_WIDTH = 3.54f;
+	public static final float SPOT_HEIGHT = 2.4f;
+
+	public static final float LEFT_LANE_POSITION_X = 0.55f;
+	public static final float CENTRAL_LANE_POSITION_X = 3.05f;
+	public static final float RIGHT_LANE_POSITION_X = 5.55f;
 
 	public Spot(float x, float y, float width, float height) {
 		super(x, y, width, height);
 		goForward();
-		
+
 		laneState = LaneState.CENTRAL;
 		stateTime = 0;
 	}
@@ -31,36 +31,40 @@ public class Spot extends DynamicGameObject {
 	public void update(float deltaTime, SwipeDirection swipeDirection) {
 		position.x += velocity.x * deltaTime;
 
-		switch (swipeDirection) {
-		case LEFT:
-			velocity.x = -SPOT_VELOCITY;
-			spotState = SpotState.GOING_LEFT;
-			stateTime = 0;
+		// Only apply spot movement (given the swipe) if it is going forward, otherwise let it finish that movement (ignoring the swipe)
+		if (spotState == SpotState.GOING_FORWARD) { 
 			
-			break;
+			switch (swipeDirection) {
+			case LEFT:
+				velocity.x = -SPOT_VELOCITY;
+				spotState = SpotState.GOING_LEFT;
+				stateTime = 0;
 
-		case RIGHT:
-			velocity.x = SPOT_VELOCITY;
-			spotState = SpotState.GOING_RIGHT;
-			stateTime = 0;
-			
-			break;
+				break;
 
-		case UP:
-			spotState = SpotState.JUMPING;
+			case RIGHT:
+				velocity.x = SPOT_VELOCITY;
+				spotState = SpotState.GOING_RIGHT;
+				stateTime = 0;
 
-			break;
+				break;
 
-		case DOWN:
-			spotState = SpotState.CROUCHING;
+			case UP:
+				spotState = SpotState.JUMPING;
 
-			break;
+				break;
 
-		case NONE:
-			break;
+			case DOWN:
+				spotState = SpotState.CROUCHING;
 
+				break;
+
+			case NONE:
+				break;
+			}
 		}
 
+		// Update spot position based on its state (where it is going)
 		switch (spotState) {
 		case GOING_RIGHT:
 
@@ -92,7 +96,7 @@ public class Spot extends DynamicGameObject {
 			break;
 
 		case GOING_LEFT:
-			
+
 			switch (laneState) {
 			case CENTRAL:
 				if (position.x <= LEFT_LANE_POSITION_X) {
@@ -102,7 +106,7 @@ public class Spot extends DynamicGameObject {
 				}
 
 				break;
-				
+
 			case LEFT:
 				position.x = LEFT_LANE_POSITION_X;
 				goForward();
@@ -133,7 +137,7 @@ public class Spot extends DynamicGameObject {
 
 		stateTime += deltaTime;
 	}
-	
+
 	private void goForward() {
 		velocity.x = 0;
 		spotState = SpotState.GOING_FORWARD;
