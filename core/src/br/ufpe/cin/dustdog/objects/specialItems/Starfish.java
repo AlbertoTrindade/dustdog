1434ea42a6e934a766a1903b6bd02ce1ab5e7639
AlbertoTrindade <att@cin.ufpe.br>
2015-02-21 01:45:08 -0300

    Increase starfish dimensions

--- core/src/br/ufpe/cin/dustdog/objects/specialItems/Starfish.java
+++ core/src/br/ufpe/cin/dustdog/objects/specialItems/Starfish.java
@@ -3,8 +3,8 @@ package br.ufpe.cin.dustdog.objects.specialItems;
 import br.ufpe.cin.dustdog.world.World;
 
 public class Starfish extends SpecialItem {
-	public static final float STARFISH_WIDTH = 1.1f;
-	public static final float STARFISH_HEIGHT = 1.09f;
+	public static final float STARFISH_WIDTH = 1.155f;
+	public static final float STARFISH_HEIGHT = 1.1445f;
 	
 	public static final float LEFT_LANE_POSITION_X = 2f;
 	public static final float CENTRAL_LANE_POSITION_X = 4.2f;
